8d4f3c7720baa8da067c26dc4de737bad0e9feaf
Philip Ashlock <phil@ashlock.us>
2016-12-22 17:09:11 -0500

    YAML fields for graph title, tier, custodian

--- sdg-yaml-convert.php
+++ sdg-yaml-convert.php
@@ -26,6 +26,17 @@ if(!empty($arguments['output'])) {
 }
 
 
+//$parse_file = file_get_contents('./data/source/master_indicator_database.csv');
+//$csv = array_map("str_getcsv", preg_split('/\r*\n+|\r+/', $parse_file));
+
+$csv = array();
+if (($handle = fopen('./data/source/master_indicator_database.csv', "r")) !== FALSE) {
+  while (($row = fgetcsv($handle, 0, ",")) !== FALSE) {
+    $csv[] = $row;
+  }
+  fclose($handle);
+}
+
 // Load list of markdown files
 $markdown_files = scandir($arguments['input']);
 
@@ -43,8 +54,44 @@ foreach ($markdown_files as $key => $filename) {
     $yaml = $document->getYAML();
     $markdown = $document->getContent();
     
-    $yaml_text = "---\n" . Yaml::dump($yaml) . "---\n";
     
+    $new_values = array('graph_title' => null, 'un_designated_tier' => null, 'un_custodial_agency' => null);
+
+    // Look for new values from spreadsheet
+    foreach ($csv as $key => $indicator) {
+
+        // Skip first row
+        if ($key < 1) {
+            continue;
+        }
+
+        if ($indicator[2] == $yaml['indicator']) {
+
+            $new_values['graph_title'] = (!empty($indicator[38])) ? $indicator[38] : null;
+
+            $new_values['un_designated_tier'] = (!empty($indicator[25])) ? $indicator[25] : null;
+            $new_values['un_custodial_agency'] = (!empty($indicator[26])) ? $indicator[26] : null;
+
+        }
+
+    }
+    reset($csv);
+
+    // inject new values into YAML array
+    $yaml_beginning = array_slice( $yaml , 0 , 7, true );
+    $yaml_end = array_slice( $yaml , 7 , NULL, true );
+    $yaml_middle = array('graph_title' => $new_values['graph_title']);
+
+    $yaml = array_merge($yaml_beginning, $yaml_middle, $yaml_end);
+
+    $yaml_beginning = array_slice( $yaml , 0 , 12, true );
+    $yaml_end = array_slice( $yaml , 12 , NULL, true );
+    $yaml_middle = array('un_designated_tier' => $new_values['un_designated_tier'], 'un_custodial_agency' => $new_values['un_custodial_agency']);
+
+    $yaml = array_merge($yaml_beginning, $yaml_middle, $yaml_end);
+
+    $yaml_text = "---\n" . Yaml::dump($yaml) . "---\n" . $markdown;
+
     file_put_contents($output_path . '/' . $filename, $yaml_text);
     $count++;
 }
