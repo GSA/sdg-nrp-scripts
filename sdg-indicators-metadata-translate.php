fb2d754ddc115bab0b419e77fd4ee731867d3e90
Philip Ashlock <phil@ashlock.us>
2017-06-27 11:32:44 -0400

    Script and data for indicator spanish translation

---
+++ sdg-indicators-metadata-translate.php
@@ -0,0 +1,85 @@
+<?php 
+
+
+$parse_file = file_get_contents('./data/source/es/sdg_indicator_metadata.csv');
+$csv = array_map("str_getcsv", preg_split('/\r*\n+|\r+/', $parse_file));
+
+
+//var_dump($csv); exit;
+
+foreach ($csv as $key => $indicator) {
+	
+	// Skip first row
+	if ($key < 1) {
+        continue;
+	}
+
+    
+
+    $goal_num = $indicator[2];
+    
+    $indicator_id = $indicator[6];
+    $indicator_title = addcslashes($indicator[7], '"');
+    $indicator_short = slugify($indicator[6]);
+    $indicator_definition = addcslashes(str_replace(".''", '.', preg_replace( "/\r|\n/", "", stripcslashes($indicator[10]))), '"');
+    $indicator_method = addcslashes(preg_replace( "/\r|\n/", "", stripcslashes($indicator[11]) ), '"' );
+    $indicator_target = addcslashes(preg_replace( "/\r|\n/", "", stripcslashes($indicator[5]) ), '"' );
+
+    $target_id = $indicator[0];
+
+    $frontmatter = "---\n";
+    $frontmatter .= "title: \"$indicator_title\"\n";
+    $frontmatter .= "lang: es\n";
+    $frontmatter .= "permalink: /es/$indicator_short/\n";
+    $frontmatter .= "sdg_goal: $goal_num\n";
+    $frontmatter .= "layout: indicator\n";
+
+    $frontmatter .= "indicator: \"$indicator_id\"\n";
+    $frontmatter .= "target_id: \"$target_id\"\n";
+
+
+    $frontmatter .= "---\n\n";
+
+    $text = $frontmatter . "";
+
+    $filename = $indicator_short;
+
+    $file = fopen($filename . '.md', 'w');
+    fwrite($file, $text);
+    fclose($file);
+
+
+}
+
+
+
+function slugify($text) {
+  // replace non letter or digits by -
+  $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+  // transliterate
+  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+  // remove unwanted characters
+  $text = preg_replace('~[^-\w]+~', '', $text);
+
+  // trim
+  $text = trim($text, '-');
+
+  // remove duplicate -
+  $text = preg_replace('~-+~', '-', $text);
+
+  // lowercase
+  $text = strtolower($text);
+
+  if (empty($text)) {
+    return 'n-a';
+  }
+
+  return $text;
+}
+
+
+
+
+?>
